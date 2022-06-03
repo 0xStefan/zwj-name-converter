5a1e5f50fc3e47bfde8fbf0608f0f22f083e7390
0xStefan <61558712+0xStefan@users.noreply.github.com>
2022-06-03 08:27:21 +0200

    Add new tests

--- test.js
+++ test.js
@@ -2,13 +2,17 @@ const test = require("ava");
 const punycodeRegex = require("./scripts/punycode-regex");
 const nameToAscii = require("./scripts/name-to-ascii");
 const nameToUnicode = require("./scripts/name-to-unicode");
+const invalidPunycodeExamples = require("./lib/invalid-punycode-examples");
 
 test("Punycode Regex", (t) => {
+  t.false(punycodeRegex().test("notpunycode"));
   t.true(punycodeRegex().test("xn--vi8h"));
   t.true(punycodeRegex().test("XN--nn8hea944cfa"));
-  t.false(punycodeRegex().test("Xn---nn8hea944cfa"));
-  t.false(punycodeRegex().test("notpunycode"));
+  t.true(punycodeRegex().test("Xn---nn8hea944cfa"));
   t.true(punycodeRegex().test("xn--vi8h🍕"));
+  t.true(punycodeRegex().test("xn----bi3sea"));
+  t.true(punycodeRegex().test("xn----7sbbqcdlv9ch"));
+  t.true(punycodeRegex().test("xn---hren---sgen-ocb8v"));
 });
 
 test("Convert Input to clean ASCII Handshake Name", (t) => {
@@ -22,10 +26,13 @@ test("Convert Input to clean ASCII Handshake Name", (t) => {
   t.is(nameToAscii("xn--vi8h🍕"), "xn--xn--vi8h-wh25g");
   t.is(nameToAscii("xn--vi8h文教材"), "xn--xn--vi8h-5x3qlog6n");
   t.is(nameToAscii("&/#,+()$~%.':*?!<>{}[]xn--vi8h🍕"), "xn--xn--vi8h-wh25g");
-  t.is(nameToAscii("xn--zugaaaa"), "xn--zugaaaa");
+  t.is(nameToAscii(".֩"), "xn--0cb");
+  t.is(nameToAscii("❤‍🫑"), "xn--qei1760n");
+  t.is(nameToAscii("❤🫑"), "xn--qei1760n");
 });
 
-test("Convert Input to Unicode Handshake name including zwj", (t) => {
+test("Convert Input to IDNA2003/2008 Unicode name including zwj", (t) => {
+  t.is(nameToUnicode("xn---hren---sgen-ocb8v"), "-hören---sägen");
   t.is(nameToUnicode("ALICE bob &/#,+()$~%.':*?!<>{}[]"), "alicebob");
   t.is(nameToUnicode("&/#, +()$~%.':*?!<>{}[]A_L IC E-bob"), "a_lice-bob");
   t.is(nameToUnicode("&/#,+()$~%.':*?!<>{}[]🍕"), "🍕");
@@ -38,17 +45,18 @@ test("Convert Input to Unicode Handshake name including zwj", (t) => {
   t.is(nameToUnicode("I love 🍕"), "ilove🍕");
   t.is(nameToUnicode("With Ümlaut ö"), "withümlautö");
   t.is(nameToUnicode("👨🏾❤👨🏽"), "👨🏾‍❤‍👨🏽");
+  t.is(nameToUnicode("xn--xn--vi8h-wh25g"), "xn--vi8h🍕");
+  t.is(nameToUnicode("xn--vi8h文教材"), "xn--vi8h文教材");
+  t.is(nameToUnicode("&/#,+()$~%.':*?!<>{}[]xn--vi8h🍕"), "xn--vi8h🍕");
+  t.is(nameToUnicode("xn----7sbbqcdlv9ch"), "аскер-заде");
+  t.is(nameToUnicode("xn--4ug3ew87z"), "‹牋‐");
+  t.is(nameToUnicode("xn--071a"), "虐");
+  t.is(nameToUnicode("xn--qei1760n"), "❤‍🫑");
+  t.is(nameToUnicode("xn----bi3sea"), "🔙-🔚");
+  t.is(nameToUnicode("xn--qei0070n"), "❤‍🫧");
 });
 
-test("Don't convert invalid punycode", (t) => {
-  t.is(nameToUnicode("xn--zugaaaa"), "xn--zugaaaa");
-  t.is(nameToUnicode("xn--1ug5408p"), "xn--1ug5408p");
-  t.is(nameToUnicode("xn--lxg0205p"), "xn--lxg0205p");
-  t.is(nameToUnicode("xn--lxg2706p"), "xn--lxg2706p");
-  t.is(nameToUnicode("xn--v86c5408p"), "xn--v86c5408p");
-  t.is(nameToUnicode("xn--lug"), "xn--lug");
-  t.is(nameToUnicode("xn--3ug"), "xn--3ug");
-  t.is(nameToUnicode("xn--vi8h🍕"), "xn--xn--vi8h-wh25g");
-  t.is(nameToUnicode("xn--vi8h文教材"), "xn--xn--vi8h-5x3qlog6n");
-  t.is(nameToUnicode("&/#,+()$~%.':*?!<>{}[]xn--vi8h🍕"), "xn--xn--vi8h-wh25g");
+test("Don't convert unsupported punycode", (t) => {
+  invalidPunycodeExamples.forEach((e) => t.is(nameToUnicode(e), e));
+  invalidPunycodeExamples.forEach((e) => t.is(nameToAscii(e), e));
 });
